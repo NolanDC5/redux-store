56bfadc55ab0116d85bc2108c8a1e9b47830ca2e
NolanDC5 <nolan.dalcorobbo@gmail.com>
2022-08-07 22:47:41 -0400

    final code

---
+++ client/build/precache-manifest.34f5d7978c6cabcef1a79996aadee474.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c8ba13b0377cc9c5c7d10e401c4213",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baa9da6f924e2590755e",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "eed7053ccba522c21720",
+    "url": "/static/js/2.dd67b8b7.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.dd67b8b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baa9da6f924e2590755e",
+    "url": "/static/js/main.8cc112d8.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
